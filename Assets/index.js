0f103a8d4bc798e8e5a2b4cc07dd94f1b298bd20
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-03-01 21:53:54 -0800

    adding first functions

--- Assets/index.js
+++ Assets/index.js
@@ -10,22 +10,69 @@ const connection = mysql.createConnection({
     database: "empTrackerDb"
 });
 
+// general entry point to the app
 connection.connect(function(err){
-    if (err) {
-        throw err;
-    }
-    selectAllFromEmp();
-    function selectAllFromEmp() {
-    var query = "SELECT * FROM employee ";
-    connection.query(query, function(err, res) {
-      if (err) throw err;
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i]);
-      }
-     
-    });
-  }
+    if (err) throw err;
+    startApp();
 });
 
+
+
+//main function
+function startApp() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "Add departments, roles, employees",
+          "View departments, roles, employees",
+          "Update employee roles",
+          "Update employee managers",
+          "View employees by manager ",
+          "Delete departments, roles, and employees",
+          "View the total utilized budget of a department -- ie the combined salaries of all employees in that department",
+          "exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "Add departments, roles, employees":
+          addDepRolesEmps();
+          break;
+  
+        case "View departments, roles, employees":
+          viewDepRolesEmps();
+          break;
+  
+        case  "Update employee roles":
+         updateEmpRoles();
+          break;
+  
+        case "Update employee managers":
+          updateEmpManagers();
+          break;
+
+          case "View employees by manager ":
+            viewEmpByManager();
+            break;
+
+        case "Delete departments, roles, and employees":
+                deleteDepRolesEmps();
+                break;
+
+        case "View the total utilized budget of a department -- ie the combined salaries of all employees in that department":
+        totalBudgetByDept();
+        break;
+
+        case "exit":
+          connection.end();
+          break;
+        }
+      });
+  }
+  
+//first case  * Add departments, roles, employees
 
 
